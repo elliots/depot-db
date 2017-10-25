81c41446970eea97252fa76c26503a8843bbbb7c
Andrew Youngwerth <drew.youngwerth@versabuilt.com>
2017-10-25 15:48:56 -0600

    Extract backup into its own utility

--- src/backup.ts
+++ src/backup.ts
@@ -1,59 +1,52 @@
-import level = require("level");
+import { Depot } from "./depot";
 import fs = require("fs");
 import split = require("split");
 import through = require("through");
 import zlib = require("zlib");
 
-export class DepotBackupEngine {
-    private readonly db: level.LevelUp<{}, {}, {}, {}>;
-
-    constructor(db: level.LevelUp<{}, {}, {}, {}>) {
-        this.db = db;
-    }
-
-    /**
-     * Backup the db to a gzip file
-     * @param location - Location to write backup to (Should end in .gz)
-     */
-    async backup(location: fs.PathLike): Promise<void> {
-        return new Promise<void>((resolve, reject) => {
-            this.createBackupStream()
-                .pipe(fs.createWriteStream(location))
-                .once("close", resolve)
-                .once("error", reject);
-        })
-    }
-
-    /**
-     * Load the db from a backup.gz
-     * @param location Location of the backup gz
-     */
-    async loadBackup(location: fs.PathLike): Promise<void> {
-        return this.loadFromBackupStream(fs.createReadStream(location, "utf8"));
-    }
-
-    /**
-     * Load the gziped backup from a stream
-     */
-    loadFromBackupStream(source: NodeJS.ReadableStream): Promise<void> {
-        let dbChain = Promise.resolve();
-        source
-            .pipe(zlib.createGunzip())
-            .pipe(split())
-            .pipe(through((str: string) => {
-                const data = JSON.parse(str);
-                dbChain = dbChain.then(() => this.db.put(data.key, data.value));
-            }))
-        return dbChain;
-    }
-
-    /** Returns a gzip backup stream */
-    createBackupStream(): NodeJS.ReadableStream {
-        return this.db.createReadStream()
-            .pipe(through(function(this: any, data: any) {
-                this.queue(JSON.stringify(data) + "\n");
-            }))
-            .pipe(zlib.createGzip());
-    }
-
+/**
+ * Backup the db to a gzip file
+ * @param location - Location to write backup to (Should end in .gz)
+ */
+function backup(db: Depot<any>, location: fs.PathLike): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+        createBackupStream(db)
+            .pipe(fs.createWriteStream(location))
+            .once("close", resolve)
+            .once("error", reject);
+    })
 }
+
+/**
+ * Load the db from a backup.gz
+ * @param location Location of the backup gz
+ */
+function loadBackup(db: Depot<any>, location: fs.PathLike): Promise<void> {
+    return loadFromBackupStream(db, fs.createReadStream(location, "utf8"));
+}
+
+/**
+ * Load the gziped backup from a stream
+ */
+function loadFromBackupStream(db: Depot<any>, source: NodeJS.ReadableStream): Promise<void> {
+    let dbChain = Promise.resolve();
+    source
+        .pipe(zlib.createGunzip())
+        .pipe(split())
+        .pipe(through((str: string) => {
+            const data = JSON.parse(str);
+            dbChain = dbChain.then(() => db.put(data.key, data.value));
+        }))
+    return dbChain;
+}
+
+/** Returns a gzip backup stream */
+function createBackupStream(db: Depot<any>): NodeJS.ReadableStream {
+    return db.createReadStream()
+        .pipe(through(function(this: any, data: any) {
+            this.queue(JSON.stringify(data) + "\n");
+        }))
+        .pipe(zlib.createGzip());
+}
+
+export const utils = { backup, loadBackup, loadFromBackupStream, createBackupStream };
